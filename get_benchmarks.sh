3961d9ffd715fd86e2d0e61bc3b8d5c47cd12cca
Erez Karpas <karpase@gmail.com>
2021-04-29 06:07:09 +0000

    benchmarks use new script

--- get_benchmarks.sh
+++ get_benchmarks.sh
@@ -2,8 +2,13 @@
 
 for x in `cat problems.txt`; do 
 	if test -f "${x%/*}/goals.txt"; then 
-		echo "(ulimit -v 16777216 -t 3600; cd "${x%/*}/"; ~/grs_compilation/run_grs_compilation.sh domain.pddl ${x##*/} goals.txt)"; 
-		echo "(ulimit -v 16777216 -t 3600; cd ~/GRS_0.1; python goal-related-states.py -d ${x%/*}/domain.pddl -p $x -g ${x%/*}/goals.txt -e optimal-plan -a all-reachable)";
+		echo "(ulimit -v 16777216 -t 3600; python3 ~/grs_compilation/grs_solve.py -d  ${x%/*}/domain.pddl -p $x -g ~/grs_compilation/${x%/*}/goals.txt.nocost -s centroid > ${x%/*}/rungrs.centroid.txt)";
+		echo "(ulimit -v 16777216 -t 3600; python3 ~/grs_compilation/grs_solve.py -d  ${x%/*}/domain.pddl -p $x -g ~/grs_compilation/${x%/*}/goals.txt.nocost -s minimum-covering -m direct > ${x%/*}/rungrs.mincover.direct.txt)";
+		echo "(ulimit -v 16777216 -t 3600; python3 ~/grs_compilation/grs_solve.py -d  ${x%/*}/domain.pddl -p $x -g ~/grs_compilation/${x%/*}/goals.txt.nocost -s minimum-covering -m binary > ${x%/*}/rungrs.mincover.binary.txt)";
+
+
+#		echo "(ulimit -v 16777216 -t 3600; cd ~/GRS_0.1; python goal-related-states.py -d ${x%/*}/domain.pddl -p $x -g ~/grs_compilation/${x%/*}/goals.txt.nocost -e optimal-plan -a all-reachable)";
+#		echo "(ulimit -v 16777216 -t 3600; cd ~/GRS_0.1; python goal-related-states.py -d ${x%/*}/domain.pddl -p $x -g ~/grs_compilation/${x%/*}/goals.txt.nocost -e optimal-plan -a all-reachable -s minimum-covering)";
 	fi  ; 
 done
 
