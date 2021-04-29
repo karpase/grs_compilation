21194f8a1dc478be9ba4a958135d273b60ace461
Erez Karpas <karpase@gmail.com>
2021-04-29 06:02:09 +0000

    made script to run GRS

--- grs_solve.py
+++ grs_solve.py
@@ -4,6 +4,7 @@ import os
 import optparse
 import copy
 import math
+import time
 
 # We get the parameters
 def parse_options():
@@ -14,9 +15,11 @@ def parse_options():
     parser.add_option('-p', '--problem-file', action='store', dest='problem_file', help='problem file')
     parser.add_option('-d', '--domain-file', action='store', dest='domain_file', help='domain file')
     parser.add_option('-g', '--goals-file', action='store', dest='goals_file', help='goals file')
-    
     parser.add_option('-c', '--cost-factor', action='store', dest='cost_factor', help='cost factor', default=10**6, type="float")
     parser.add_option('-f', '--fd-cmd', action='store', dest='fd_cmd', help='Fast Downward Command', default="~/fd/fast-downward.py --alias seq-opt-lmcut", type="string")
+    parser.add_option('-m', '--min-cover-comp', action='store', dest='min_cover_comp', help='Minimum Cover Compilation (direct/binary)', default="direct", type="string")
+
+
 
 
     options, args = parser.parse_args()
@@ -25,7 +28,10 @@ def parse_options():
 def call_planner(options, budget):
     print("Creating compilation for budget", budget)
 
-    cmd = "python3 grs_compilation.py -s " + options.state + " -d " + options.domain_file + " -p " + options.problem_file + " -g " + options.goals_file + " -c " + str(options.cost_factor) + " -b " + str(budget)
+    if budget >= 0:
+        cmd = "python3 grs_compilation.py -s " + options.state + " -d " + options.domain_file + " -p " + options.problem_file + " -g " + options.goals_file + " -c " + str(options.cost_factor) + " -b " + str(budget)
+    else:
+        cmd = "python3 grs_compilation.py -s " + options.state + " -d " + options.domain_file + " -p " + options.problem_file + " -g " + options.goals_file + " -c " + str(options.cost_factor)
     os.system(cmd)
 
     print("Calling planner for budget", budget)
@@ -38,12 +44,7 @@ def call_planner(options, budget):
         return False
 
 
-
-def main():
-    options = parse_options()
-
-    assert(options.state == 'minimum-covering' or options.state == 'minimum-covering-m')
-
+def binary_search_minimum_covering(options):
     # First search - increase until finding first solution
     budget_lb = 0 # highest budget that failed
     budget_ub = 0 # lowest budget that succeeded
@@ -70,11 +71,26 @@ def main():
         else:
             budget_lb = budget
 
-    print("Minimum covering: ", budget_ub)
     return budget_ub
 
 
 
+def main():
+    options = parse_options()
+
+
+    t1 = time.time()
+
+    if options.min_cover_comp == "binary":
+        assert(options.state == 'minimum-covering' or options.state == 'minimum-covering-m')
+        val = binary_search_minimum_covering(options)
+    else:
+        val = call_planner(options, -1)
+
+    t2 = time.time()
+
+
+    print("*****", options.domain_file, options.problem_file, options.goals_file, options.state, options.min_cover_comp, val, t2-t1)
 
 
 if __name__ == '__main__':
